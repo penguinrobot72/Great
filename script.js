212790046fd03a7ee7ebc5bd8717773e7786919e
penguin robot <sophie960826@gmail.com>
2022-12-09 22:24:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-alert("count down！！ 5")
-alert("count down！！ 4")
-alert("count down！！ 3")
-alert("count down！！ 2")
-alert("count down！！ 1")
+alert("count down！！ 5");
+alert("count down！！ 4");
+alert("count down！！ 3");
+alert("count down！！ 2");
+alert("count down！！ 1");
